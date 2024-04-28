c1d8106ed364a5f85c755bae92a658e7d13d2279
Mahipal2005 <mgckurchhi@gmail.com>
2024-04-28 18:01:37 +0530

    28/04/2024 evu file add

---
+++ evutlation-28/04/index.js
@@ -0,0 +1,126 @@
+let main = document.querySelector("#main");
+let container = document.querySelector("#container");
+let tbody = document.querySelector("tbody");
+let Department = document.querySelector("#Department");
+let Gender = document.querySelector("#Gender");
+let salary = document.querySelector("#salary");
+let url =
+  "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees";
+var currentPage = 1;
+var rowsPerPage = 10;
+var startrowpage = 0;
+
+function nextPage() {
+  currentPage = currentPage + 1;
+  startrowpage = startrowpage + 10;
+  rowsPerPage += 10;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      showdata(data.data);
+    });
+}
+
+function prevPage() {
+  currentPage = currentPage - 1;
+  startrowpage = startrowpage - 10;
+  rowsPerPage -= 10;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      showdata(data.data);
+    });
+}
+function showdata(data) {
+  tbody.innerHTML = "";
+  data.forEach((element, i) => {
+    if (rowsPerPage > i && startrowpage <= i) {
+      let tr = document.createElement("tr");
+      let Sno = document.createElement("td");
+      Sno.innerHTML = element.id;
+      let name = document.createElement("td");
+      name.innerHTML = element.name;
+      let genders = document.createElement("td");
+      genders.innerHTML = element.gender;
+      let department = document.createElement("td");
+      department.innerHTML = element.department;
+      let salary = document.createElement("td");
+      salary.innerHTML = element.salary;
+
+      tr.appendChild(Sno);
+      tr.appendChild(name);
+      tr.appendChild(genders);
+      tr.appendChild(department);
+      tr.appendChild(salary);
+      tbody.appendChild(tr);
+    }
+  });
+}
+
+let display = async () => {
+  let res = await fetch(url);
+  let data = await res.json();
+  showdata(data.data);
+};
+display();
+
+Department.addEventListener("change", () => {
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let fullData = data.data;
+      if (Department.value === "all") {
+        showdata(fullData);
+      } else {
+        let filterData = fullData.filter((element) => {
+          return element.department == Department.value;
+        });
+        showdata(filterData);
+      }
+    });
+});
+
+Gender.addEventListener("change", () => {
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let fullData = data.data;
+      if (Gender.value === "all") {
+        showdata(fullData);
+      } else {
+        let filterData = fullData.filter((element) => {
+          return element.gender == Gender.value;
+        });
+        showdata(filterData);
+      }
+    });
+});
+salary.addEventListener("change", () => {
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let fullData = data.data;
+      if (salary.value === "all") {
+        showdata(fullData);
+      } else {
+        let value = salary.value;
+        let narr;
+        if (value == "LowToHigh") {
+          narr = fullData.sort((a, b) => a.salary - b.salary);
+        } else {
+          narr = fullData.sort((a, b) => b.salary - a.salary);
+        }
+        showdata(narr);
+      }
+    });
+});
